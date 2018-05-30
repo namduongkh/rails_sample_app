5c5b4177fea93e18760ccb0dce6ae0225276894f
Phong Nguyễn <namduong.kh94@gmail.com>
2018-05-30 21:36:52 +0700

    Phong Nguyễn’s comment:

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 VALID_PASSWORD_REGEX = /[\w]+/i
 
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save   :downcase_email
     before_create :create_activation_digest
     # before_save { self.email = email.downcase }
@@ -56,6 +56,23 @@ class User < ApplicationRecord
         UserMailer.account_activation(self).deliver_now
     end
 
+    # Sets the password reset attributes.
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest,  User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # Sends password reset email.
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    # Returns true if a password reset has expired.
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
+    
     private
     # Converts email to all lower-case.
     def downcase_email
